2118099e0e9ed9ca186aaa7a97699754158250ba
priyesh16@gmail.com <priyesh16@gmail.com>
2016-01-14 10:16:22 -0800

    Added README file

--- Assignment1/src/server/server.c
+++ Assignment1/src/server/server.c
@@ -1,29 +1,93 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <common.h>
 
-#define BACKLOG 1
+#define USAGE "\n ./myserver <port no (1-65535)> \n"
 
-/* return a listen socket bound on  given port */
-int getlistensocket(unsigned long port) {
-	int sockfd = 0, connfd = 0;
-	int ret;
-	struct sockaddr_in serv_addr; 
+int main(int argc, char **argv){
+	int ret = SUCCESS; 
+	int err = SUCCESS; 
+	unsigned long port;
+	int sockfd;
+	char cmd[CMDLEN];
+	int saved_stdout;
+	FILE *dummyfp;
+	char output[OUTLEN];
+	ssize_t n;
+	int nextclient = 1;
+	socklen_t len = sizeof(err);
 
-	/* fill socket data structure given a port */
-	memset(&serv_addr, '0', sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(port); 
+	bzero(cmd, sizeof(cmd));
 
-	/* listen to client and return an accepted socket */
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
-	Bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-	Listen(sockfd, BACKLOG);
-	connfd = Accept(sockfd, (struct sockaddr*)NULL, NULL);
+	/* validate and retrieve the port from input arguments
+	 * and open a socket bound to the port
+	 */
+	validatearg(argc, 2, USAGE);
+	retrieveport(argv[1], &port);
 
-	/* close listening socket and return the client socket */
+	/*
+	 * connect to a client, accept commands from it
+	 * and sent it the output of the command
+	 */
+		
+	while(1){
+		len = sizeof(err);
+		err = SUCCESS; 
+		/* connect to a new client */
+		if (nextclient == 1) {
+			sockfd = getlistensocket(port);
+			nextclient = 0;
+		}
+
+		/* read command to be executed from the client, if
+		 * read fails for reasons like the client hit Ctrl+C
+		 * then connect to next client.
+		 */
+		printf(PROMPT);
+		fflush(stdout);
+		if((n = read(sockfd, cmd, sizeof(cmd) - 1)) <= 0){
+			close(sockfd);
+			perror("read:");
+			nextclient = 1;
+			continue;
+		}
+		cmd[n] = '\0';
+		printf("%s\n", cmd);
+
+		/* if exit command was sent close connection with 
+		 * the current client and open a new connection
+		 */
+		n = strcmp(cmd, EXITCMD);
+		if (n == 0) {
+			nextclient = 1;
+			close(sockfd);
+			continue;
+		}
+		
+		/* if command doesn't have produce an output then
+		 * let the client know
+		 */
+		dummyfp = popen(cmd, "r");
+		if(!fgets(output, sizeof(output), dummyfp)){
+			write(sockfd, NOOUT, strlen(NOOUT)); 
+			write(sockfd, ACK, strlen(ACK)); 
+			continue;
+		}
+		pclose(dummyfp);
+
+		/* print command output on server side also */
+		system(cmd);
+
+		/* instead of writing output to stdout write 
+		 * directly to socket and then send an ack
+		 */
+		saved_stdout = dup(STDOUT_FILENO);
+		dup2(sockfd, STDOUT_FILENO);
+		system(cmd);
+		sendack();
+		dup2(saved_stdout, STDOUT_FILENO);
+		close(saved_stdout);
+	}
+
+	/* close socket descriptor and exit */
 	close(sockfd);
-	return connfd;
+	exit(SUCCESS);
 }
